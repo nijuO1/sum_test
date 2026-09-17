209c7fa01c55bd7f6a7470db14afaf51d067859c
niju <niju@niju.net>
2026-09-17 11:19:19 +0900

    Add main.cpp

--- sum.h
+++ sum.h
@@ -1,9 +1,8 @@
-// sum.cpp
+// main.cpp
+#include <stdio.h>
 #include "sum.h"
 
-int sum(int n) {
-	int res = 0;
-	for (int i = 1; i <= n; ++i)
-		res += i;
-	return res;
+int main() {
+	int s = sum(10);
+	printf("result=%d\n", s);
 }
